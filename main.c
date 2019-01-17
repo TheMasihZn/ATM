829c379113780abb9437baf235bf61ea121a29b8
mahdiabbaszadeh <46725350+mahdiabbaszadeh@users.noreply.github.com>
2019-01-17 16:26:32 +0330

    +user

--- main.c
+++ main.c
@@ -4,16 +4,19 @@
 //#include <curses.h>
 
 char LANGUAGE = 'F',tmp=' ';
-int choice, tempPass[5], password[5];
+int choice, tempPass[5], password[5], moj = 10000000,gozineh,halgheh;
 
-int stringCMP(int *a, int *b);
+int moghayeseh(int *a, int *b);
 void out(int);
 void language(char *);
-void userChangePassword();
-void adminMenu();
+
+void ChangePassword();
 
 
-void userMenu();
+void enteghaleVajh();
+
+
+void daryaftVajh();
 
 main() {
 
@@ -24,20 +27,77 @@ main() {
     language(&LANGUAGE);
 //    system("cls");
      out(3);
+
     for (int j = 0; j < 4; ++j) {
         scanf("%1d", &password[j]);
     }
-    userMenu();
+    do{
+    out(9);
     out(2);             //choice
     scanf("%d", &choice);
 
+        switch (choice) {
+            case 1:
+                daryaftVajh();
+                break;
+            case 2:
+                break;
+            case 3:
+                enteghaleVajh();
+                break;
+            case 4:
+                ChangePassword();
+                break;
+            case 5:
+                out(18);
+                break;
+
+
+
+
+            default:
+                break;
+        }
+
+
+
+        }while(halgheh==1);}
+
+void daryaftVajh() {
+    out(10);
+    int temp, gozineh, halgheh;
+    scanf("%d", &temp);
+    // system("@cls||clear");
+    if (temp > moj)
+        out(11);
+    else
+        out(12);
+    //getch();
+    //system("cls");
+    moj = moj - temp;
+    out(13);
+    scanf("%d", &gozineh);
+    //system("cls");
+    if (gozineh == 1) {
+        if (LANGUAGE == 'F')
+            printf("\n\n shomareh kart: 6104 3378 **** 4260\n mablagh daryafti:%d rial \n mojoodi:%d rial", temp, moj);
+        else if (LANGUAGE == 'E')
+            printf("\n\n kart number: 6104 3378 **** 4260\n:%d rial\n mojoodi:%d rial", temp, moj);
+        // getch();
+    }
+        out(14);                    ///tasmim edameh
+        scanf("%d", &halgheh);
+        //system("cls");
+
+
 
 }
 
-void userMenu() {
 
 
-}
+
+
+
 
 void language(char *l) {
     int i;
@@ -49,7 +109,7 @@ void language(char *l) {
     }
 }
 
-int stringCMP(int *a, int *b) {
+int moghayeseh(int *a, int *b) {
     for (int i = 0; i < 4; ++i) {
         if (a[i] != b[i]) {
             return 0;
@@ -116,6 +176,38 @@ void out(int code) {
                 case 8:
                     printf("Ramz eshtebah ast.\n");
                     break;
+                case 9:
+                    printf("\n\n 1.daryaafte Vajh\t\t2.Daryafte Mojoodi\n\n3.Enteghale Vajh\t\t4.Tghire ramz\n\n5.Sarfe nazar az anjame amaleiyat\n\n6.Sepordeh gozari\t\t7.Gereftane soorat hesab\n\ngozineh morede nazar ra vared konid:  ");
+                    break;
+                case 10:
+                    printf("mablaghe morede nazar be rial:    ");
+                    break;
+                case 11:
+                    printf("\n mojoodi kafi nist. ");
+                    break;
+                case 12:
+                    printf("lotfan vajh khod ra daryaft konid");
+                    break;
+                case 13:
+                    printf("\n\n 1.Bale \n 2.Kheir\n aya resid mikhahid?  ");
+                    break;
+                case 14:
+                    printf("\n\n 1. Bale \n 2. Kheir \n aya mikhahid edameh bedaheid؟ ");
+                    break;
+                case 15:
+                    printf("Shomare Kart Maghsad Ra Vared Konid: ");
+                    break;
+                case 16:
+                    printf("Mablagh Morede nazar Ba Movaffaghiyat enteghal yaft.");
+                    break;
+                case 17:
+                    printf("\nKarte mabda:");
+                    break;
+                case 18:
+                    printf("\n Lotfan kart khod ra bardarid.\nsepas gozarim baraye entekhab in aberbank.");
+
+
+                    break;
                 default:
                     printf("Undefined!");
             }
@@ -123,38 +215,76 @@ void out(int code) {
     }
 }
 
-void userChangePassword() {
+void ChangePassword(){
 
     int bool = 1;
     while (bool) {
         out(5);
         for (int i = 0; i < 4; i++) {
             scanf("%1d", &tempPass[i]);
-        }
+                                    }
 
-        if (stringCMP(tempPass, password)) {
+        if (moghayeseh(tempPass, password)) {
             bool = 0;
             out(6);
             for (int j = 0; j < 4; ++j) {
                 scanf("%1d", &password[j]);
             }
             out(7);
-        } else {
+                                           } else {
             out(8);
-        }
+                                                  }
 
+
+                 }
+    out(14);                    ///tasmim edameh
+    scanf("%d", &halgheh);
+
+}
+
+void enteghaleVajh() {
+    int i, kartmaghsad[16];
+    out(15);
+    for (i = 0; i < 16; i++)
+        scanf("%1d", &kartmaghsad[i]);
+    //system("cls");
+    out(10);
+    int temp;
+    scanf("%d", &temp);
+    moj -= temp;
+    //system("cls");
+    if (temp > moj)
+        out(11);
+    else {
+        out(16);
+        //getch();
+        //system("cls");
+        out(13);
+        scanf("%d", &gozineh);
     }
+    if (gozineh == 1)
+        if (LANGUAGE == 'F')
+            printf("\t\t\tBANK PASARGAD\n\t\t\tKode Shobeh:3921\nTarikh:1397/10/30\nZaman:10:00:00\nVajhe enteghali:%d\nKarte maghsad:",
+                   temp);
+        else if (LANGUAGE == 'E')
+            printf("\t\t\tBANK PASARGAD\n\t\t\tcode branch:3921\ndate:1397/10/30\ntime:10:00:00\n cash move:%d\ntarget card:",
+                   temp);
+    for (i = 0; i < 16; i++)
+        printf("%1d", kartmaghsad[i]);
+    out(17);
+    if (LANGUAGE == 'F')
+        printf("\nMojodi:%d", moj);
+    else if (LANGUAGE == 'E')
+        printf("");
+    //getch();
+    // system("cls");
+    out(14);
+    scanf("%d", &halgheh);
+    //system("cls");
+                }
 
-}
-
-void adminMenu() {
 
 
-
-
-
-
-}
 
 
 
